bb7af0ab6777076855b77a729da41ce12c84a32d
Sally Crawford <sally.crawford@gmail.com>
2019-04-14 15:43:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,18 +1,20 @@
 def using_push(array,string)
   updated_array = array.push(string)
-  return updated_array
 end
 
 def using_unshift(array,string)
   updated_array = array.unshift(string)
-  return updated_array
 end
 
 def using_pop(array)
   updated_array = array.pop
-  return updated_array
 end
 
 def pop_with_args(array)
   updated_array = array.pop(2)
 end
+
+def using_shift(array)
+  updated_array = array.shift
+end
+
