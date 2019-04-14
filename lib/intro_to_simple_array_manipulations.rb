a107bd9b9008b2608b19674bf3b29852368ba48d
Sally Crawford <sally.crawford@gmail.com>
2019-04-14 15:34:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,8 @@ def using_push(array,string)
   return updated_array
 end
 
-it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
+it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
   updated_array = using_push(countries_in_western_africa, next_country)
   expect(updated_array.last).to eq(next_country)
-  
