405cea8a09a94b50ab7e0ec17f6f46af85fe40e5
Sally Crawford <sally.crawford@gmail.com>
2019-04-14 15:37:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,9 @@ def using_push(array,string)
   updated_array = array.push(string)
   return updated_array
 end
+
+def using_unshift(array,string)
+  updated_array = array.unshift(string)
+  return updated_array
+end
+  
